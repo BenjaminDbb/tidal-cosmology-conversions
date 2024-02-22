653e8b626ac2b555eef1bd698cde06f45e179b04
Junjie Zhao <BenjaminDbb@users.noreply.github.com>
2024-02-22 16:14:49 +0800

    fix the wrong equation in the code

--- t_cosmo/lambda_k_relations.py
+++ t_cosmo/lambda_k_relations.py
@@ -1,30 +1,28 @@
-import pkg_resources
-
 import numpy as np
 from scipy.special import gamma
 
 n = 0.8
-a_k_i = np.loadtxt(
-    pkg_resources.resource_filename(__name__,  '/data/fit_values.txt')
+
+a_k_i = np.asarray(
+    [
+        [-0.349395404871373,  -1.0629526931221451,  -1.8203302183430596],
+        [5.674192626267164,   10.648790155995867,   14.693288291531053 ],
+        [0.2957502016916985,  1.8002037299447995,   2.741120521996333  ],
+    ]
 ).T
 
-def get_lambda_0_k(k):
-    def _get_lambda_k(lambda_0_0):
-        a_i = a_k_i[k - 1]
-        lam_minus_one_fifth = lambda_0_0**(-1./5.)
-        lam_terms = np.array([lam_minus_one_fifth,
-                              lam_minus_one_fifth**2,
-                              lam_minus_one_fifth**3])
-        prefactor = gamma(k + 10./(3. - n))/gamma(10./(3. - n))
-        postfactor = np.dot(a_i, lam_terms)
-        return prefactor * lambda_0_0 * postfactor
-    return _get_lambda_k
-
+def get_lambda_0_k(k, lambda_0_0):
+    if k == 0:
+        return lambda_0_0
+    a_i = a_k_i[k - 1]
+    x = lambda_0_0**(-1./5.)
+    lam_terms = np.array([x, x**2, x**3])
+    G_n_k = gamma(k + 10./(3. - n))/gamma(10./(3. - n))
+    return G_n_k * lambda_0_0 * (1 + np.dot(a_i, lam_terms))
+   
 def get_lambda_from_mass(source_frame_mass, lambda_0_0, M0=1.4):
     """Based on Yagi & Yunes (2018) expansion (Eq. 22)"""
-    lambda_tot = lambda_0_0
-
-    for k, lam_k in enumerate(map(get_lambda_0_k, (1, 2, 3))):
-        r = lam_k(lambda_0_0) * (1 - source_frame_mass/M0)**(k + 1)
-        lambda_tot += r
+    lambda_tot = 0.0
+    for k in range(4): # k in [0, 1, 2, 3]
+        lambda_tot += get_lambda_0_k(k=k, lambda_0_0=lambda_0_0) * (1 - source_frame_mass/M0)**(k) / np.math.factorial(k)
     return lambda_tot
